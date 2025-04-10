bbe7b5878c4e9854e0000a4d562b63d3172cfe32
waleedsamoul <168515280+waleedsamoul@users.noreply.github.com>
2025-04-10 15:11:42 +0200

    LäggTillLagerhattar

---
+++ Hattmakarens system/Presentationslager/LaggTillLagerhattar.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Hattmakarens_system.Presentationslager
+{
+    partial class LaggTillLagerhattar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Studentmössa;
+            pictureBox1.Location = new Point(64, 97);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(303, 293);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.Hatt_med_fjäder;
+            pictureBox3.Location = new Point(674, 97);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(303, 293);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.Fez_hatt;
+            pictureBox4.Location = new Point(995, 97);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(257, 293);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.sherlock_hatt;
+            pictureBox5.Location = new Point(1268, 97);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(319, 293);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Doktormössa;
+            pictureBox2.Location = new Point(373, 97);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(295, 293);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(124, 403);
+            label1.Name = "label1";
+            label1.Size = new Size(164, 32);
+            label1.TabIndex = 5;
+            label1.Text = "Studentmössa";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(439, 403);
+            label2.Name = "label2";
+            label2.Size = new Size(154, 32);
+            label2.TabIndex = 6;
+            label2.Text = "Doktormössa";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(750, 403);
+            label3.Name = "label3";
+            label3.Size = new Size(143, 32);
+            label3.TabIndex = 7;
+            label3.Text = "Oktoberhatt";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1062, 403);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 32);
+            label4.TabIndex = 8;
+            label4.Text = "Fez hatt";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1343, 403);
+            label5.Name = "label5";
+            label5.Size = new Size(154, 32);
+            label5.TabIndex = 9;
+            label5.Text = "Sherlock hatt";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(124, 499);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 10;
+            button1.Text = "Lägg till";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(439, 499);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 46);
+            button2.TabIndex = 11;
+            button2.Text = "Lägg till";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(743, 499);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 46);
+            button3.TabIndex = 12;
+            button3.Text = "Lägg till";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1043, 499);
+            button4.Name = "button4";
+            button4.Size = new Size(150, 46);
+            button4.TabIndex = 13;
+            button4.Text = "Lägg till";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(157, 447);
+            label6.Name = "label6";
+            label6.Size = new Size(80, 32);
+            label6.TabIndex = 14;
+            label6.Text = "800 kr";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(474, 447);
+            label7.Name = "label7";
+            label7.Size = new Size(80, 32);
+            label7.TabIndex = 15;
+            label7.Text = "400 kr";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(780, 447);
+            label8.Name = "label8";
+            label8.Size = new Size(93, 32);
+            label8.TabIndex = 16;
+            label8.Text = "1000 kr";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(1072, 447);
+            label9.Name = "label9";
+            label9.Size = new Size(80, 32);
+            label9.TabIndex = 17;
+            label9.Text = "300 kr";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(1371, 447);
+            label10.Name = "label10";
+            label10.Size = new Size(93, 32);
+            label10.TabIndex = 18;
+            label10.Text = "1200 kr";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(1343, 499);
+            button5.Name = "button5";
+            button5.Size = new Size(150, 46);
+            button5.TabIndex = 19;
+            button5.Text = "Lägg till";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // LaggTillLagerhattar
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1678, 838);
+            Controls.Add(button5);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "LaggTillLagerhattar";
+            Text = "LaggTillLagerhattar";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Button button5;
+    }
+}
