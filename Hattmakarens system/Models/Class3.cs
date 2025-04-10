877b3e214ea44a3a9ac6f62ac5450b23df1fd879
Cowwax <Max.cowlin04@gmail.com>
2025-04-10 11:51:52 +0200

    +

---
+++ Hattmakarens system/Models/Class3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hattmakarens_system.Models
+{
+    internal class OrderRad
+    {
+        [Key]
+    public int OrderRadId { get; set; }
+    public HattEnum Typ { get; set; }
+
+     public decimal Pris { get; set; }
+        
+     public blabla Status { get; set; }
+
+     public string? ReferensBild { get; set; }
+
+        [ForeignKey(nameof(AnsvarigUserId))]
+        public string AnsvarigUserId { get; set; }
+        [ForeignKey(nameof(HattModellId)]
+        public string HattModellId { get; set;}
+
+
+
+
+
+
+
+
+
+
+    }
+    public enum HattEnum
+    {
+        Lager,
+        Special
+    }
+}
---
+++ Hattmakarens system/Models/Kund.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hattmakarens_system.Models
+{
+    internal class Class1
+    {
+    }
+}
