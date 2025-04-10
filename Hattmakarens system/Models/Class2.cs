f0338280ab3d9f6aa5787f29a53ac326271e31b2
Cowwax <Max.cowlin04@gmail.com>
2025-04-10 11:52:35 +0200

    Skapat databas klasser
    Tjoho

--- Hattmakarens system/Models/Class2.cs
+++ Hattmakarens system/Models/Class2.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hattmakarens_system.Models
 {
-    internal class Class2
+    internal class Frakt
     {
+
+        [Key]
+        public int FraktId { get; set; }
+        public int ExportKod { get; set; }
+        public int Vikt { get; set; }
+        public decimal Värde { get; set; }
+        public double Moms { get; set; }
+        public decimal PrisInkMoms { get; set; }
+        public DateTime SkapatDatum { get; set; }
+        [ForeignKey(nameof(OrderID))]
+        public int OrderID { get; set; }
+
+
     }
 }
--- Hattmakarens system/Models/Kund.cs
+++ Hattmakarens system/Models/Kund.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hattmakarens_system.Models
 {
-    internal class Class1
+    public class Kund
     {
+        [Key]
+        public int KundId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string ForNamn { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string EfterNamn { get; set; }
+        [Phone]
+        [MaxLength(50)]
+        public string Telefon { get; set; }
+        [EmailAddress]
+        [MaxLength(200)]
+        public string Epost { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Adress { get; set; }
+        public bool Aktiv { get; set; }
+
+
     }
+
 }
