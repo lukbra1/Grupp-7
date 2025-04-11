059776a19fb7ae15a7d88ed51e9c12ec5a5d1819
Midvinternatt <43345799+Midvinternatt@users.noreply.github.com>
2025-04-11 15:18:23 +0200

    Skapade ny klass KundController

---
+++ Hattmakarens system/Controllers/KundController.cs
@@ -0,0 +1,40 @@
+﻿using Hattmakarens_system.Database;
+using Hattmakarens_system.ModelsNy;
+
+namespace Hattmakarens_system.Controllers
+{
+    internal class KundController
+    {
+        private readonly AppDbContext _context;
+
+        public KundController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public Kund SkapaNyKund(string Fornamn, string Efternamn, string TelefonNr, string Adress)
+        {
+            Kund kund = new Kund {
+                Fornamn = Fornamn,
+                Efternamn = Efternamn,
+                TelefonNr = TelefonNr,
+                Adress = Adress,
+                //Ordrar = new List<Order>(),
+                Aktiv = true
+            };
+            _context.SaveChanges();
+            return kund;
+        }
+        public List<Kund> AllaAktivaKunder()
+        {
+            return null; //_context.Kunder.Where(k => k.Aktiv).ToList();
+        }
+        public Kund HamtaKundFranFornamn(string ForNamn)
+        {
+            return null; //this._context.Kunder.FirstOrDefault(k => k.ForNamn == ForNamn);
+        }
+        public void RaderaKund(Kund kund)
+        {
+            //_context.Kunder.Remove(kund);
+        }
+    }
+}
