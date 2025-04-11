366da3eb17494adea24f2303ab111e4ab8674a2e
mursal2251436 <mursal.mohammad@gmail.com>
2025-04-11 16:07:31 +0200

    Reg ny kund

---
+++ Hattmakarens system/RegistreraKund.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Hattmakarens_system
+{
+    partial class RegistreraKund
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFörnamn = new Label();
+            txtEfternamn = new Label();
+            txtTelefon = new Label();
+            txtEpost = new Label();
+            txtAdress = new Label();
+            txtForNamn = new TextBox();
+            txtEfterName = new TextBox();
+            txtTel = new TextBox();
+            txtEmail = new TextBox();
+            txtAddress = new TextBox();
+            lblRubrik = new Label();
+            lblFel1 = new Label();
+            lblFel2 = new Label();
+            lblFel3 = new Label();
+            lblFel4 = new Label();
+            lblFel6 = new Label();
+            btnRegistrera = new Button();
+            SuspendLayout();
+            // 
+            // txtFörnamn
+            // 
+            txtFörnamn.AutoSize = true;
+            txtFörnamn.Location = new Point(93, 46);
+            txtFörnamn.Name = "txtFörnamn";
+            txtFörnamn.Size = new Size(55, 15);
+            txtFörnamn.TabIndex = 0;
+            txtFörnamn.Text = "Förnamn";
+            // 
+            // txtEfternamn
+            // 
+            txtEfternamn.AutoSize = true;
+            txtEfternamn.Location = new Point(93, 92);
+            txtEfternamn.Name = "txtEfternamn";
+            txtEfternamn.Size = new Size(62, 15);
+            txtEfternamn.TabIndex = 1;
+            txtEfternamn.Text = "Efternamn";
+            // 
+            // txtTelefon
+            // 
+            txtTelefon.AutoSize = true;
+            txtTelefon.Location = new Point(93, 145);
+            txtTelefon.Name = "txtTelefon";
+            txtTelefon.Size = new Size(45, 15);
+            txtTelefon.TabIndex = 2;
+            txtTelefon.Text = "Telefon";
+            // 
+            // txtEpost
+            // 
+            txtEpost.AutoSize = true;
+            txtEpost.Location = new Point(100, 190);
+            txtEpost.Name = "txtEpost";
+            txtEpost.Size = new Size(36, 15);
+            txtEpost.TabIndex = 3;
+            txtEpost.Text = "Epost";
+            // 
+            // txtAdress
+            // 
+            txtAdress.AutoSize = true;
+            txtAdress.Location = new Point(93, 237);
+            txtAdress.Name = "txtAdress";
+            txtAdress.Size = new Size(42, 15);
+            txtAdress.TabIndex = 4;
+            txtAdress.Text = "Adress";
+            // 
+            // txtForNamn
+            // 
+            txtForNamn.Location = new Point(184, 43);
+            txtForNamn.Name = "txtForNamn";
+            txtForNamn.Size = new Size(100, 23);
+            txtForNamn.TabIndex = 5;
+            // 
+            // txtEfterName
+            // 
+            txtEfterName.Location = new Point(184, 89);
+            txtEfterName.Name = "txtEfterName";
+            txtEfterName.Size = new Size(100, 23);
+            txtEfterName.TabIndex = 6;
+            // 
+            // txtTel
+            // 
+            txtTel.Location = new Point(184, 137);
+            txtTel.Name = "txtTel";
+            txtTel.Size = new Size(100, 23);
+            txtTel.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(184, 187);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(100, 23);
+            txtEmail.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(184, 234);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(100, 23);
+            txtAddress.TabIndex = 9;
+            // 
+            // lblRubrik
+            // 
+            lblRubrik.AutoSize = true;
+            lblRubrik.Location = new Point(159, 9);
+            lblRubrik.Name = "lblRubrik";
+            lblRubrik.Size = new Size(105, 15);
+            lblRubrik.TabIndex = 10;
+            lblRubrik.Text = "Registrera ny kund";
+            // 
+            // lblFel1
+            // 
+            lblFel1.AutoSize = true;
+            lblFel1.Location = new Point(314, 46);
+            lblFel1.Name = "lblFel1";
+            lblFel1.Size = new Size(0, 15);
+            lblFel1.TabIndex = 11;
+            // 
+            // lblFel2
+            // 
+            lblFel2.AutoSize = true;
+            lblFel2.Location = new Point(314, 89);
+            lblFel2.Name = "lblFel2";
+            lblFel2.Size = new Size(0, 15);
+            lblFel2.TabIndex = 12;
+            // 
+            // lblFel3
+            // 
+            lblFel3.AutoSize = true;
+            lblFel3.Location = new Point(314, 145);
+            lblFel3.Name = "lblFel3";
+            lblFel3.Size = new Size(0, 15);
+            lblFel3.TabIndex = 13;
+            // 
+            // lblFel4
+            // 
+            lblFel4.AutoSize = true;
+            lblFel4.Location = new Point(314, 195);
+            lblFel4.Name = "lblFel4";
+            lblFel4.Size = new Size(0, 15);
+            lblFel4.TabIndex = 14;
+            // 
+            // lblFel6
+            // 
+            lblFel6.AutoSize = true;
+            lblFel6.Location = new Point(314, 242);
+            lblFel6.Name = "lblFel6";
+            lblFel6.Size = new Size(0, 15);
+            lblFel6.TabIndex = 15;
+            // 
+            // btnRegistrera
+            // 
+            btnRegistrera.Location = new Point(129, 263);
+            btnRegistrera.Name = "btnRegistrera";
+            btnRegistrera.Size = new Size(75, 23);
+            btnRegistrera.TabIndex = 16;
+            btnRegistrera.Text = "Registrera";
+            btnRegistrera.UseVisualStyleBackColor = true;
+            btnRegistrera.Click += btnRegistrera_Click;
+            // 
+            // RegistreraKund
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1035, 450);
+            Controls.Add(btnRegistrera);
+            Controls.Add(lblFel6);
+            Controls.Add(lblFel4);
+            Controls.Add(lblFel3);
+            Controls.Add(lblFel2);
+            Controls.Add(lblFel1);
+            Controls.Add(lblRubrik);
+            Controls.Add(txtAddress);
+            Controls.Add(txtEmail);
+            Controls.Add(txtTel);
+            Controls.Add(txtEfterName);
+            Controls.Add(txtForNamn);
+            Controls.Add(txtAdress);
+            Controls.Add(txtEpost);
+            Controls.Add(txtTelefon);
+            Controls.Add(txtEfternamn);
+            Controls.Add(txtFörnamn);
+            Name = "RegistreraKund";
+            Text = "RegistreraKund";
+            TransparencyKey = Color.FromArgb(255, 128, 128);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label txtFörnamn;
+        private Label txtEfternamn;
+        private Label txtTelefon;
+        private Label txtEpost;
+        private Label txtAdress;
+        private TextBox txtForNamn;
+        private TextBox txtEfterName;
+        private TextBox txtTel;
+        private TextBox txtEmail;
+        private TextBox txtAddress;
+        private Label lblRubrik;
+        private Label lblFel1;
+        private Label lblFel2;
+        private Label lblFel3;
+        private Label lblFel4;
+        private Label lblFel6;
+        private Button btnRegistrera;
+    }
+}
---
+++ Hattmakarens system/RegistreraKund.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mail;
+using System.Windows.Forms;
+using Hattmakarens_system.Database;
+using Hattmakarens_system.Models;
+
+namespace Hattmakarens_system
+{
+    public partial class RegistreraKund : Form
+    {
+        public RegistreraKund()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ClearErrorMessages()
+        {
+            lblFel1.Text = string.Empty;
+            lblFel2.Text = string.Empty;
+            lblFel3.Text = string.Empty;
+            lblFel4.Text = string.Empty;
+            lblFel6.Text = string.Empty;
+        }
+
+        // (Valfritt) Event-handlers om du vill lägga till något:
+        private void RegistreraKund_Load(object sender, EventArgs e) { }
+        private void txtForNamn_TextChanged(object sender, EventArgs e) { }
+        private void txtEfterName_TextChanged(object sender, EventArgs e) { }
+
+        private void btnRegistrera_Click(object sender, EventArgs e)
+        {
+            using (var context = new AppDbContext())
+            {
+                string epost = txtEmail.Text.Trim();
+
+                bool epostFinns = context.Kunder.Any(k => k.Epost == epost);
+                if (epostFinns)
+                {
+                    lblFel4.Text = "E-postadressen är redan registrerad, ange en annan E-postadress.";
+                    return; // Stoppa registreringen
+                }
+            }
+
+
+            if (!ValidateFields())
+                return;
+
+            Kund nyKund = new Kund
+            {
+                ForNamn = txtForNamn.Text,
+                EfterNamn = txtEfterName.Text,
+                Telefon = txtTel.Text,
+                Epost = txtEmail.Text,
+                Adress = txtAddress.Text,
+            };
+
+            try
+            {
+                using (var context = new AppDbContext())
+                {
+                    context.Kunder.Add(nyKund);
+                    context.SaveChanges();
+                }
+
+                MessageBox.Show("Kunden har sparats i databasen!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ett fel inträffade: {ex.Message}", "Fel", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidateFields()
+        {
+            bool harFel = false;
+
+            // Förnamn
+            if (string.IsNullOrWhiteSpace(txtForNamn.Text))
+            {
+                lblFel1.Text = "Förnamn måste fyllas i.";
+                harFel = true;
+            }
+            else
+            {
+                if (txtForNamn.Text.Length < 2)
+                {
+                    lblFel1.Text = "Förnamnet måste innehålla minst 2 bokstäver.";
+                    harFel = true;
+                }
+                else if (txtForNamn.Text.Length > 50)
+                {
+                    lblFel1.Text = "Förnamnet får inte vara längre än 50 tecken.";
+                    harFel = true;
+                }
+                else if (!txtForNamn.Text.All(char.IsLetter))
+                {
+                    lblFel1.Text = "Förnamnet får endast innehålla bokstäver.";
+                    harFel = true;
+                }
+            }
+
+            // Efternamn
+            if (string.IsNullOrWhiteSpace(txtEfterName.Text))
+            {
+                lblFel2.Text = "Efternamn måste fyllas i.";
+                harFel = true;
+            }
+            else
+            {
+                if (txtEfterName.Text.Length < 2)
+                {
+                    lblFel2.Text = "Efternamnet måste innehålla minst 2 bokstäver.";
+                    harFel = true;
+                }
+                else if (txtEfterName.Text.Length > 50)
+                {
+                    lblFel2.Text = "Efternamnet får inte vara längre än 50 tecken.";
+                    harFel = true;
+                }
+                else if (!txtEfterName.Text.All(char.IsLetter))
+                {
+                    lblFel2.Text = "Efternamnet får endast innehålla bokstäver.";
+                    harFel = true;
+                }
+            }
+
+            // Telefon
+            if (string.IsNullOrWhiteSpace(txtTel.Text))
+            {
+                lblFel3.Text = "Telefonnummer måste fyllas i.";
+                harFel = true;
+            }
+            else
+            {
+                if (txtTel.Text.Length > 50)
+                {
+                    lblFel3.Text = "Telefonnummer får inte vara längre än 50 tecken.";
+                    harFel = true;
+                }
+                else if (!IsValidPhoneNumber(txtTel.Text))
+                {
+                    lblFel3.Text = "Telefonnumret är inte giltigt.";
+                    harFel = true;
+                }
+            }
+
+            // E-post
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                lblFel4.Text = "E-postadress måste fyllas i.";
+                harFel = true;
+            }
+            else
+            {
+                if (txtEmail.Text.Length > 200)
+                {
+                    lblFel4.Text = "E-postadressen får inte vara längre än 200 tecken.";
+                    harFel = true;
+                }
+                else if (!IsValidEmail(txtEmail.Text))
+                {
+                    lblFel4.Text = "E-postadressen är inte giltig.";
+                    harFel = true;
+                }
+            }
+
+            // Adress
+            if (string.IsNullOrWhiteSpace(txtAddress.Text))
+            {
+                lblFel6.Text = "Adress måste fyllas i.";
+                harFel = true;
+            }
+            else
+            {
+                if (txtAddress.Text.Length < 10)
+                {
+                    lblFel6.Text = "Adressen måste innehålla minst 10 tecken.";
+                    harFel = true;
+                }
+                else if (!txtAddress.Text.All(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)))
+                {
+                    lblFel6.Text = "Adressen får endast innehålla bokstäver, siffror och mellanslag.";
+                    harFel = true;
+                }
+            }
+
+
+            return !harFel;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            try
+            {
+                string cleaned = phoneNumber.Replace(" ", "").Replace("-", "");
+                if (cleaned.StartsWith("+"))
+                    cleaned = cleaned.Substring(1);
+
+                return cleaned.All(char.IsDigit) && cleaned.Length >= 7 && cleaned.Length <= 15;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+     
+    }
+}
+
