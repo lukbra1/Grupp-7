2fd8eb9f7ae38c88c5a1bf5eef64b57321553fa0
LinnNord <linn.nord7@gmail.com>
2025-04-14 14:27:11 +0200

    +

--- Hattmakarens system/Migrations/20250414122650_max.Designer.cs
+++ Hattmakarens system/Migrations/20250414122650_max.Designer.cs
@@ -4,6 +4,7 @@ using Hattmakarens_system.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hattmakarens_system.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250414122650_max")]
+    partial class max
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hattmakarens system/Migrations/20250414122650_max.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hattmakarens_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class max : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dekoration",
+                table: "Orderrader");
+
+            migrationBuilder.DropColumn(
+                name: "ExtraMaterial",
+                table: "Orderrader");
+
+            migrationBuilder.DropColumn(
+                name: "Farg",
+                table: "Orderrader");
+
+            migrationBuilder.DropColumn(
+                name: "Fjader",
+                table: "Orderrader");
+
+            migrationBuilder.DropColumn(
+                name: "Modell",
+                table: "Orderrader");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Orderrader");
+
+            migrationBuilder.RenameColumn(
+                name: "Tyg",
+                table: "Orderrader",
+                newName: "Referensbild");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Storlek",
+                table: "Orderrader",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Orderrader",
+                keyColumn: "OrderRadId",
+                keyValue: 1,
+                column: "Storlek",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Referensbild",
+                table: "Orderrader",
+                newName: "Tyg");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Storlek",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dekoration",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExtraMaterial",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Farg",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fjader",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Modell",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Orderrader",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
