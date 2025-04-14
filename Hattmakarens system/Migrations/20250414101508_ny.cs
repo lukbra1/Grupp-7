5c742428fa6e448b005a1e9768db03650bec44c4
lukbra1 <152890173+lukbra1@users.noreply.github.com>
2025-04-14 12:16:00 +0200

    ny migration

---
+++ Hattmakarens system/Migrations/20250414101508_ny.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hattmakarens_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class ny : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TypEnum",
+                table: "Orderrader",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Orderrader",
+                keyColumn: "OrderRadId",
+                keyValue: 1,
+                column: "TypEnum",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypEnum",
+                table: "Orderrader");
+        }
+    }
+}
