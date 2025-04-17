25664df1bb2c454d0fbad42bb6d66442440b3635
mursal2251436 <mursal.mohammad@gmail.com>
2025-04-17 12:14:07 +0200

    BeställMaterial

--- Hattmakarens system/Presentationslager/SeMaterialbehov.Designer.cs
+++ Hattmakarens system/Presentationslager/SeMaterialbehov.Designer.cs
@@ -28,91 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            grpBoxSeHatt = new GroupBox();
-            btnVisaMaterial = new Button();
-            cmbHattar = new ComboBox();
             dataGridViewMaterial = new DataGridView();
-            btnSparaBestallning = new Button();
-            grpBoxSeHatt.SuspendLayout();
+            dtpSlutDatum = new DateTimePicker();
+            dtpStartDatum = new DateTimePicker();
+            lblFrån = new Label();
+            lblTill = new Label();
+            btnFiltreraDatum = new Button();
+            label3 = new Label();
+            lvOrdrar = new ListView();
+            btnExporteraMaterial = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridViewMaterial).BeginInit();
             SuspendLayout();
-            // 
-            // grpBoxSeHatt
-            // 
-            grpBoxSeHatt.Controls.Add(btnVisaMaterial);
-            grpBoxSeHatt.Controls.Add(cmbHattar);
-            grpBoxSeHatt.Location = new Point(14, 37);
-            grpBoxSeHatt.Margin = new Padding(3, 4, 3, 4);
-            grpBoxSeHatt.Name = "grpBoxSeHatt";
-            grpBoxSeHatt.Padding = new Padding(3, 4, 3, 4);
-            grpBoxSeHatt.Size = new Size(229, 133);
-            grpBoxSeHatt.TabIndex = 0;
-            grpBoxSeHatt.TabStop = false;
-            grpBoxSeHatt.Text = "Lista över hattar ";
-            // 
-            // btnVisaMaterial
-            // 
-            btnVisaMaterial.Location = new Point(8, 68);
-            btnVisaMaterial.Margin = new Padding(3, 4, 3, 4);
-            btnVisaMaterial.Name = "btnVisaMaterial";
-            btnVisaMaterial.Size = new Size(137, 31);
-            btnVisaMaterial.TabIndex = 1;
-            btnVisaMaterial.Text = "Visa Materialbehov";
-            btnVisaMaterial.UseVisualStyleBackColor = true;
-            // 
-            // cmbHattar
-            // 
-            cmbHattar.FormattingEnabled = true;
-            cmbHattar.Location = new Point(7, 29);
-            cmbHattar.Margin = new Padding(3, 4, 3, 4);
-            cmbHattar.Name = "cmbHattar";
-            cmbHattar.Size = new Size(138, 28);
-            cmbHattar.TabIndex = 0;
             // 
             // dataGridViewMaterial
             // 
             dataGridViewMaterial.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewMaterial.Location = new Point(347, 37);
-            dataGridViewMaterial.Margin = new Padding(3, 4, 3, 4);
+            dataGridViewMaterial.Location = new Point(575, 12);
             dataGridViewMaterial.Name = "dataGridViewMaterial";
             dataGridViewMaterial.RowHeadersWidth = 51;
-            dataGridViewMaterial.Size = new Size(882, 492);
+            dataGridViewMaterial.Size = new Size(580, 413);
             dataGridViewMaterial.TabIndex = 1;
             // 
-            // btnSparaBestallning
+            // dtpSlutDatum
             // 
-            btnSparaBestallning.Location = new Point(1101, 487);
-            btnSparaBestallning.Margin = new Padding(3, 4, 3, 4);
-            btnSparaBestallning.Name = "btnSparaBestallning";
-            btnSparaBestallning.Size = new Size(129, 43);
-            btnSparaBestallning.TabIndex = 2;
-            btnSparaBestallning.Text = "Spara beställning";
-            btnSparaBestallning.UseVisualStyleBackColor = true;
-            btnSparaBestallning.Click += btnSparaBestallning_Click;
+            dtpSlutDatum.Location = new Point(144, 487);
+            dtpSlutDatum.Name = "dtpSlutDatum";
+            dtpSlutDatum.Size = new Size(103, 23);
+            dtpSlutDatum.TabIndex = 3;
+            // 
+            // dtpStartDatum
+            // 
+            dtpStartDatum.Location = new Point(12, 487);
+            dtpStartDatum.Name = "dtpStartDatum";
+            dtpStartDatum.Size = new Size(114, 23);
+            dtpStartDatum.TabIndex = 4;
+            // 
+            // lblFrån
+            // 
+            lblFrån.AutoSize = true;
+            lblFrån.Location = new Point(57, 469);
+            lblFrån.Name = "lblFrån";
+            lblFrån.Size = new Size(30, 15);
+            lblFrån.TabIndex = 5;
+            lblFrån.Text = "Från";
+            // 
+            // lblTill
+            // 
+            lblTill.AutoSize = true;
+            lblTill.Location = new Point(184, 469);
+            lblTill.Name = "lblTill";
+            lblTill.Size = new Size(22, 15);
+            lblTill.TabIndex = 6;
+            lblTill.Text = "Till";
+            // 
+            // btnFiltreraDatum
+            // 
+            btnFiltreraDatum.Location = new Point(270, 487);
+            btnFiltreraDatum.Name = "btnFiltreraDatum";
+            btnFiltreraDatum.Size = new Size(75, 23);
+            btnFiltreraDatum.TabIndex = 7;
+            btnFiltreraDatum.Text = "Filtrera";
+            btnFiltreraDatum.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(101, 226);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 8;
+            // 
+            // lvOrdrar
+            // 
+            lvOrdrar.Location = new Point(12, 12);
+            lvOrdrar.Name = "lvOrdrar";
+            lvOrdrar.Size = new Size(447, 413);
+            lvOrdrar.TabIndex = 9;
+            lvOrdrar.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnExporteraMaterial
+            // 
+            btnExporteraMaterial.Location = new Point(746, 487);
+            btnExporteraMaterial.Name = "btnExporteraMaterial";
+            btnExporteraMaterial.Size = new Size(162, 23);
+            btnExporteraMaterial.TabIndex = 10;
+            btnExporteraMaterial.Text = "Skriv ut materialbehov";
+            btnExporteraMaterial.UseVisualStyleBackColor = true;
             // 
             // SeMaterialbehov
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1166, 631);
-            Controls.Add(btnSparaBestallning);
+            ClientSize = new Size(1235, 522);
+            Controls.Add(btnExporteraMaterial);
+            Controls.Add(lvOrdrar);
+            Controls.Add(label3);
+            Controls.Add(btnFiltreraDatum);
+            Controls.Add(lblTill);
+            Controls.Add(lblFrån);
+            Controls.Add(dtpStartDatum);
+            Controls.Add(dtpSlutDatum);
             Controls.Add(dataGridViewMaterial);
-            Controls.Add(grpBoxSeHatt);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "SeMaterialbehov";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "SeMaterialbehov&Beställningsstatus";
-            grpBoxSeHatt.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridViewMaterial).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private GroupBox grpBoxSeHatt;
-        private ComboBox cmbHattar;
-        private Button btnVisaMaterial;
         private DataGridView dataGridViewMaterial;
-        private Button btnSparaBestallning;
+        private DateTimePicker dtpSlutDatum;
+        private DateTimePicker dtpStartDatum;
+        private Label lblFrån;
+        private Label lblTill;
+        private Button btnFiltreraDatum;
+        private Label label3;
+        private ListView lvOrdrar;
+        private Button btnExporteraMaterial;
     }
 }
