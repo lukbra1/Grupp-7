f81ba73e0d5ea937ceb520b455faaede69fab89d
Midvinternatt <43345799+Midvinternatt@users.noreply.github.com>
2025-04-23 15:02:10 +0200

    +

--- Hattmakarens system/Presentationslager/VäljKund.Designer.cs
+++ Hattmakarens system/Presentationslager/VäljKund.Designer.cs
@@ -58,16 +58,18 @@
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(642, 235);
+            listBox1.Location = new Point(395, 147);
+            listBox1.Margin = new Padding(2, 2, 2, 2);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(401, 228);
+            listBox1.Size = new Size(248, 144);
             listBox1.TabIndex = 28;
             // 
             // button2
             // 
-            button2.Location = new Point(955, 163);
+            button2.Location = new Point(588, 102);
+            button2.Margin = new Padding(2, 2, 2, 2);
             button2.Name = "button2";
-            button2.Size = new Size(88, 46);
+            button2.Size = new Size(54, 29);
             button2.TabIndex = 27;
             button2.Text = "Sök";
             button2.UseVisualStyleBackColor = true;
@@ -75,18 +77,19 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(642, 167);
+            textBox1.Location = new Point(395, 104);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Sök på kundens förnamn";
-            textBox1.Size = new Size(306, 39);
+            textBox1.Size = new Size(190, 27);
             textBox1.TabIndex = 26;
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
-            button1.Location = new Point(642, 501);
+            button1.Location = new Point(395, 313);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
-            button1.Size = new Size(401, 46);
+            button1.Size = new Size(247, 29);
             button1.TabIndex = 24;
             button1.Text = "Skapa beställning med vald kund";
             button1.UseVisualStyleBackColor = true;
@@ -98,24 +101,24 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { tiibakaToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Padding = new Padding(11, 4, 0, 4);
-            menuStrip1.Size = new Size(1168, 44);
+            menuStrip1.Padding = new Padding(7, 2, 0, 2);
+            menuStrip1.Size = new Size(719, 28);
             menuStrip1.TabIndex = 42;
             menuStrip1.Text = "menuStrip1";
             // 
             // tiibakaToolStripMenuItem
             // 
             tiibakaToolStripMenuItem.Name = "tiibakaToolStripMenuItem";
-            tiibakaToolStripMenuItem.Size = new Size(115, 36);
+            tiibakaToolStripMenuItem.Size = new Size(75, 24);
             tiibakaToolStripMenuItem.Text = "Tillbaka";
             tiibakaToolStripMenuItem.Click += tiibakaToolStripMenuItem_Click;
             // 
             // btnRegistrera
             // 
-            btnRegistrera.Location = new Point(166, 652);
-            btnRegistrera.Margin = new Padding(6);
+            btnRegistrera.Location = new Point(102, 408);
+            btnRegistrera.Margin = new Padding(4, 4, 4, 4);
             btnRegistrera.Name = "btnRegistrera";
-            btnRegistrera.Size = new Size(245, 49);
+            btnRegistrera.Size = new Size(151, 31);
             btnRegistrera.TabIndex = 59;
             btnRegistrera.Text = "Lägg till kund";
             btnRegistrera.UseVisualStyleBackColor = true;
@@ -124,162 +127,163 @@
             // lblFel6
             // 
             lblFel6.AutoSize = true;
-            lblFel6.Location = new Point(1102, 709);
-            lblFel6.Margin = new Padding(6, 0, 6, 0);
+            lblFel6.Location = new Point(678, 443);
+            lblFel6.Margin = new Padding(4, 0, 4, 0);
             lblFel6.Name = "lblFel6";
-            lblFel6.Size = new Size(0, 32);
+            lblFel6.Size = new Size(0, 20);
             lblFel6.TabIndex = 58;
             // 
             // lblFel4
             // 
             lblFel4.AutoSize = true;
-            lblFel4.Location = new Point(1030, 692);
-            lblFel4.Margin = new Padding(6, 0, 6, 0);
+            lblFel4.Location = new Point(634, 432);
+            lblFel4.Margin = new Padding(4, 0, 4, 0);
             lblFel4.Name = "lblFel4";
-            lblFel4.Size = new Size(0, 32);
+            lblFel4.Size = new Size(0, 20);
             lblFel4.TabIndex = 57;
             // 
             // lblFel3
             // 
             lblFel3.AutoSize = true;
-            lblFel3.Location = new Point(1030, 586);
-            lblFel3.Margin = new Padding(6, 0, 6, 0);
+            lblFel3.Location = new Point(634, 366);
+            lblFel3.Margin = new Padding(4, 0, 4, 0);
             lblFel3.Name = "lblFel3";
-            lblFel3.Size = new Size(0, 32);
+            lblFel3.Size = new Size(0, 20);
             lblFel3.TabIndex = 56;
             // 
             // lblFel2
             // 
             lblFel2.AutoSize = true;
-            lblFel2.Location = new Point(1030, 466);
-            lblFel2.Margin = new Padding(6, 0, 6, 0);
+            lblFel2.Location = new Point(634, 291);
+            lblFel2.Margin = new Padding(4, 0, 4, 0);
             lblFel2.Name = "lblFel2";
-            lblFel2.Size = new Size(0, 32);
+            lblFel2.Size = new Size(0, 20);
             lblFel2.TabIndex = 55;
             // 
             // lblFel1
             // 
             lblFel1.AutoSize = true;
-            lblFel1.Location = new Point(1030, 374);
-            lblFel1.Margin = new Padding(6, 0, 6, 0);
+            lblFel1.Location = new Point(634, 234);
+            lblFel1.Margin = new Padding(4, 0, 4, 0);
             lblFel1.Name = "lblFel1";
-            lblFel1.Size = new Size(0, 32);
+            lblFel1.Size = new Size(0, 20);
             lblFel1.TabIndex = 54;
             // 
             // lblRubrik
             // 
             lblRubrik.AutoSize = true;
-            lblRubrik.Location = new Point(216, 86);
-            lblRubrik.Margin = new Padding(6, 0, 6, 0);
+            lblRubrik.Location = new Point(133, 54);
+            lblRubrik.Margin = new Padding(4, 0, 4, 0);
             lblRubrik.Name = "lblRubrik";
-            lblRubrik.Size = new Size(213, 32);
+            lblRubrik.Size = new Size(131, 20);
             lblRubrik.TabIndex = 53;
             lblRubrik.Text = "Registrera ny kund";
             // 
             // txtAddress
             // 
-            txtAddress.Location = new Point(225, 568);
-            txtAddress.Margin = new Padding(6);
+            txtAddress.Location = new Point(138, 355);
+            txtAddress.Margin = new Padding(4, 4, 4, 4);
             txtAddress.Name = "txtAddress";
-            txtAddress.Size = new Size(182, 39);
+            txtAddress.Size = new Size(114, 27);
             txtAddress.TabIndex = 52;
             // 
             // txtEmail
             // 
-            txtEmail.Location = new Point(225, 468);
-            txtEmail.Margin = new Padding(6);
+            txtEmail.Location = new Point(138, 292);
+            txtEmail.Margin = new Padding(4, 4, 4, 4);
             txtEmail.Name = "txtEmail";
-            txtEmail.Size = new Size(182, 39);
+            txtEmail.Size = new Size(114, 27);
             txtEmail.TabIndex = 51;
             // 
             // txtTel
             // 
-            txtTel.Location = new Point(225, 361);
-            txtTel.Margin = new Padding(6);
+            txtTel.Location = new Point(138, 226);
+            txtTel.Margin = new Padding(4, 4, 4, 4);
             txtTel.Name = "txtTel";
-            txtTel.Size = new Size(182, 39);
+            txtTel.Size = new Size(114, 27);
             txtTel.TabIndex = 50;
             // 
             // txtEfterName
             // 
-            txtEfterName.Location = new Point(225, 259);
-            txtEfterName.Margin = new Padding(6);
+            txtEfterName.Location = new Point(138, 162);
+            txtEfterName.Margin = new Padding(4, 4, 4, 4);
             txtEfterName.Name = "txtEfterName";
-            txtEfterName.Size = new Size(182, 39);
+            txtEfterName.Size = new Size(114, 27);
             txtEfterName.TabIndex = 49;
             // 
             // txtForNamn
             // 
-            txtForNamn.Location = new Point(225, 161);
-            txtForNamn.Margin = new Padding(6);
+            txtForNamn.Location = new Point(138, 101);
+            txtForNamn.Margin = new Padding(4, 4, 4, 4);
             txtForNamn.Name = "txtForNamn";
-            txtForNamn.Size = new Size(182, 39);
+            txtForNamn.Size = new Size(114, 27);
             txtForNamn.TabIndex = 48;
             // 
             // lblAdress
             // 
             lblAdress.AutoSize = true;
-            lblAdress.Location = new Point(56, 575);
-            lblAdress.Margin = new Padding(6, 0, 6, 0);
+            lblAdress.Location = new Point(34, 359);
+            lblAdress.Margin = new Padding(4, 0, 4, 0);
             lblAdress.Name = "lblAdress";
-            lblAdress.Size = new Size(84, 32);
+            lblAdress.Size = new Size(53, 20);
             lblAdress.TabIndex = 47;
             lblAdress.Text = "Adress";
             // 
             // lblEpost
             // 
             lblEpost.AutoSize = true;
-            lblEpost.Location = new Point(69, 475);
-            lblEpost.Margin = new Padding(6, 0, 6, 0);
+            lblEpost.Location = new Point(42, 297);
+            lblEpost.Margin = new Padding(4, 0, 4, 0);
             lblEpost.Name = "lblEpost";
-            lblEpost.Size = new Size(72, 32);
+            lblEpost.Size = new Size(46, 20);
             lblEpost.TabIndex = 46;
             lblEpost.Text = "Epost";
             // 
             // lblTelefon
             // 
             lblTelefon.AutoSize = true;
-            lblTelefon.Location = new Point(56, 379);
-            lblTelefon.Margin = new Padding(6, 0, 6, 0);
+            lblTelefon.Location = new Point(34, 237);
+            lblTelefon.Margin = new Padding(4, 0, 4, 0);
             lblTelefon.Name = "lblTelefon";
-            lblTelefon.Size = new Size(93, 32);
+            lblTelefon.Size = new Size(58, 20);
             lblTelefon.TabIndex = 45;
             lblTelefon.Text = "Telefon";
             // 
             // lblEfternamn
             // 
             lblEfternamn.AutoSize = true;
-            lblEfternamn.Location = new Point(56, 265);
-            lblEfternamn.Margin = new Padding(6, 0, 6, 0);
+            lblEfternamn.Location = new Point(34, 166);
+            lblEfternamn.Margin = new Padding(4, 0, 4, 0);
             lblEfternamn.Name = "lblEfternamn";
-            lblEfternamn.Size = new Size(124, 32);
+            lblEfternamn.Size = new Size(77, 20);
             lblEfternamn.TabIndex = 44;
             lblEfternamn.Text = "Efternamn";
             // 
             // lblFörnamn
             // 
             lblFörnamn.AutoSize = true;
-            lblFörnamn.Location = new Point(56, 167);
-            lblFörnamn.Margin = new Padding(6, 0, 6, 0);
+            lblFörnamn.Location = new Point(34, 104);
+            lblFörnamn.Margin = new Padding(4, 0, 4, 0);
             lblFörnamn.Name = "lblFörnamn";
-            lblFörnamn.Size = new Size(109, 32);
+            lblFörnamn.Size = new Size(67, 20);
             lblFörnamn.TabIndex = 43;
             lblFörnamn.Text = "Förnamn";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(642, 86);
+            label1.Location = new Point(395, 54);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(241, 32);
+            label1.Size = new Size(149, 20);
             label1.TabIndex = 60;
             label1.Text = "Välj existerande kund";
             // 
             // VäljKund
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1168, 736);
+            ClientSize = new Size(719, 460);
             Controls.Add(label1);
             Controls.Add(btnRegistrera);
             Controls.Add(lblFel6);
@@ -303,6 +307,7 @@
             Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "VäljKund";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "VäljKund";
